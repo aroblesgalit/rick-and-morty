bc72c6687730670122bdb089b98360f43c8ee037
Alvin Galit <aroblesgalit@gmail.com>
2023-04-01 14:06:16 -0700

    Styled default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header class="container shadow-sm bg-white p-4">Header</header>
+
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
