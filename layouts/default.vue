433547df603cccf97dbbc67928d4bf7ee3cc2ab1
Alvin Galit <aroblesgalit@gmail.com>
2023-04-01 14:42:51 -0700

    Added a nav for filtering

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <header class="container shadow-sm bg-white p-4">Header</header>
+        <header class="container shadow-sm bg-white p-4 mx-auto">
+            <nav class="mx-auto flex justify-between">
+                <h1 class="font-bold text-lg text-cyan-500">Rick and Morty Characters</h1>
+                <ul class="flex gap-4">
+                    <li>All</li>
+                    <li>Rick</li>
+                    <li>Morty</li>
+                </ul>
+            </nav>
+        </header>
 
         <div class="container mx-auto p-4">
             <slot />
