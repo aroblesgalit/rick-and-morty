9dc52abeefc40c6d571a2c55a0985abcb5420424
Alvin Galit <aroblesgalit@gmail.com>
2023-04-01 13:48:24 -0700

    Integrated graphql using nuxt apollo

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
         <h1>Home</h1>
+        {{ data.characters }}
     </div>
 </template>
 
-<script setup>
-
+<script lang="ts" setup>
+const query = gql`
+    query getCharacters {
+        characters {
+            results {
+                name
+                image
+                species
+            }
+        }
+    }
+`
+const { data } = await useAsyncQuery(query)
 </script>
 
 <style scoped>
