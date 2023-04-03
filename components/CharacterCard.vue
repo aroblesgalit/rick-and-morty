69f09d49159e8e417c124d717a930b8fea665f63
Alvin Galit <aroblesgalit@gmail.com>
2023-04-02 20:38:36 -0700

    Created component for a character card

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="card">
-        Card
+        <img :src="character.image" :alt="character.name">
+        <h3>{{ character.name }}</h3>
+        <p>{{ character.species }}</p>
     </div>
 </template>
 
 <script setup>
-
+    const { character } = defineProps(['character'])
 </script>
 
 <style scoped>
-
+    img {
+        margin: 0;
+    }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,8 @@
     <div>
         <h2 class="mb-4">Home</h2>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-1 gap-y-5">
-            <div v-for="char in data.characters.results" class="card">
-                <img :src="char.image" :alt="char.name">
-                <h3>{{ char.name }}</h3>
-                <p>{{ char.species }}</p>
+            <div v-for="char in data.characters.results">
+                <CharacterCard :character="char" />
             </div>
         </div>
     </div>
@@ -27,7 +25,5 @@ const { data } = await useAsyncQuery(query)
 </script>
 
 <style scoped>
-    img {
-        margin: 0;
-    }
+    
 </style>
