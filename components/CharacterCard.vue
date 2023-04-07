579e351ad1eea27ae902041cca15759998edc53c
Alvin Galit <aroblesgalit@gmail.com>
2023-04-06 20:59:59 -0700

    Restyled card

--- components/CharacterCard.vue
+++ components/CharacterCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="rounded-lg overflow-hidden shadow-lg p-5 text-center">
-        <div class="relative flex justify-center">
-            <img class="rounded" :src="character.image" :alt="character.name">
-            <h3 class="bg-white border-solid border px-6 py-2 text-center text-lg rounded-lg w-max absolute">{{ character.name }}</h3>
+    <div class="card rounded-lg overflow-hidden shadow-md">
+        <img :src="character.image" :alt="character.name">
+        <div class="text-white px-4 pb-4">
+            <h3 class="text-lg leading-5 text-[#97ce4c] font-bold mb-1">{{ character.name }}</h3>
+            <p class="text-sm font-light">{{ character.species }}</p>
         </div>
-        <p class="mt-6">{{ character.species }}</p>
     </div>
 </template>
 
@@ -13,10 +13,27 @@
 </script>
 
 <style scoped>
+/* 
+Logo name
+blue: #00b5cc
+green: #b2df28
+
+Palette
+brown: #44281d
+peach: #e4a788
+yellow: #f0e14a
+green: #97ce4c
+pink: #e89ac7
+*/
+
+    .card {
+        background-color: #44281d;
+    }
     img {
-        max-width: 200px;
+        display: block;
+        -webkit-mask-image:-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+        mask-image: linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0));
     }
-    h3 {
-        bottom: -20px;
-    }
+
+
 </style>
