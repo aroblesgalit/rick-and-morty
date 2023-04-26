9afe72c1dce24fd839c7e8ebbe27ac68f6155a57
Alvin Galit <aroblesgalit@gmail.com>
2023-04-25 22:13:34 -0700

    Styled character detail page

--- pages/character/[id].vue
+++ pages/character/[id].vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <a href="" @click.prevent="$router.back()">Go back</a>
+        <a class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm" href="" @click.prevent="$router.back()">Go back</a>
         <div class="flex flex-col md:flex-row gap-4 mt-4">
             <div class="basis-full md:basis-1/2">
-                <img class="md:w-full" :src="characterDetail.image" :alt="characterDetail.name">
+                <img class="md:w-full rounded-lg" :src="characterDetail.image" :alt="characterDetail.name">
             </div>
-            <div class="basis-full md:basis-1/2">
-                <h2 class="text-5xl font-black">{{ characterDetail.name }}</h2>
-                <p class="text-2xl mb-4 font-bold">{{ characterDetail.species }}</p>
+            <div class="char-details basis-full md:basis-1/2 relative p-2 pl-0">
+                <h2 class="text-5xl font-black text-[#f0e14a]">{{ characterDetail.name }}</h2>
+                <p class="text-2xl mb-4 font-bold text-gray-500">{{ characterDetail.species }}</p>
                 <p class="mb-1"><span class="font-bold">Status:</span> {{ characterDetail.status }}</p>
                 <p class="mb-1"><span class="font-bold">Gender:</span> {{ characterDetail.gender }}</p>
                 <p class="mb-1"><span class="font-bold">Origin:</span> {{ characterDetail.origin.name }}</p>
@@ -33,5 +33,31 @@ setCharacterDetail(id);
 </script>
 
 <style scoped>
+    .char-details::before {
+        content: '';
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        border: 2px solid #97ce4c;
+        border-left: none;
+        border-bottom: none;
+        border-radius: 0 10px 0;
+        width: 100px;
+        height: 150px;
+    }
     
+    .char-details::after {
+        content: '';
+        display: block;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        border: 2px solid #97ce4c;
+        border-top: none;
+        border-left: none;
+        border-radius: 0 0 10px;
+        width: 150px;
+        height: 100px;
+    }
 </style>
