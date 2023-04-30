27c90f060a37d478e939af572b855b5946d36ce3
Alvin Galit <aroblesgalit@gmail.com>
2023-04-29 21:59:47 -0700

    Crated component for pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex justify-end items-center gap-x-4 my-6">
+        <div>Showing {{ (20 * pageInt) - 19 }}-{{ (20 * pageInt) - 19 + results.length - 1 }} of {{ info.count }}</div>
+        <div class="flex gap-x-2">
+            <div v-if="page > 1">
+                <NuxtLink :to="`/page/${pageInt - 1}`" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Prev</NuxtLink>
+            </div>
+            <div v-if="page < info.pages">
+                <NuxtLink :to="`/page/${pageInt + 1}`" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Next</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const { page, info, results } = defineProps(['page', 'info', 'results']);
+    let pageInt = parseInt(page);
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/page/[page].vue
+++ pages/page/[page].vue
@@ -1,17 +1,7 @@
 <template>
     <div>
         <CharacterCards :results="res" />
-        <div class="flex justify-end items-center gap-x-4 my-6">
-            <div>Showing {{ (20 * pageInt) - 19 }}-{{ (20 * pageInt) - 19 + res.length - 1 }} of {{ inf.count }}</div>
-            <div class="flex gap-x-2">
-                <div v-if="page > 1">
-                    <NuxtLink :to="`/page/${pageInt - 1}`" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Prev</NuxtLink>
-                </div>
-                <div v-if="page < inf.pages">
-                    <NuxtLink :to="`/page/${pageInt + 1}`" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Next</NuxtLink>
-                </div>
-            </div>
-        </div>
+        <Pagination :page="page" :info="inf" :results="res" />
     </div>
 </template>
 
