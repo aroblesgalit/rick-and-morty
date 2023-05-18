5a85cb6f58560ef59d9841261f26b994e5ab7e50
Alvin Galit <aroblesgalit@gmail.com>
2023-05-18 10:36:50 -0700

    Created match game header component

---
+++ components/MatchGameHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mb-6 flex justify-between">
+        <div>
+            <button @click="setCards('easy')" class="rounded bg-[#00b5cc] text-white px-2 py-1 mr-2 hover:bg-[#e89ac7] text-sm">Easy</button>
+            <button @click="setCards('medium')" class="rounded bg-[#00b5cc] text-white px-2 py-1 mr-2 hover:bg-[#e89ac7] text-sm">Medium</button>
+            <button @click="setCards('hard')" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Hard</button>
+        </div>
+        <div>{{ Math.floor(timer / 60) }}m {{ (timer % 60) < 10 ? '0' : '' }}{{ (timer % 60) }}s</div>
+    </div>
+</template>
+
+<script setup>
+const { timer, setCards } = defineProps(['timer', 'setCards'])
+</script>
+
+<style scoped>
+
+</style>
--- pages/games/matchgame.vue
+++ pages/games/matchgame.vue
@@ -1,13 +1,6 @@
 <template>
     <div class="md:max-w-3xl mx-auto">
-        <div class="mb-6 flex justify-between">
-            <div>
-                <button @click="setCards('easy')" class="rounded bg-[#00b5cc] text-white px-2 py-1 mr-2 hover:bg-[#e89ac7] text-sm">Easy</button>
-                <button @click="setCards('medium')" class="rounded bg-[#00b5cc] text-white px-2 py-1 mr-2 hover:bg-[#e89ac7] text-sm">Medium</button>
-                <button @click="setCards('hard')" class="rounded bg-[#00b5cc] text-white px-2 py-1 hover:bg-[#e89ac7] text-sm">Hard</button>
-            </div>
-            <div>{{ Math.floor(timer / 60) }}m {{ (timer % 60) < 10 ? '0' : '' }}{{ (timer % 60) }}s</div>
-        </div>
+        <MatchGameHeader :timer="timer" :setCards="setCards" />
         <MatchGameCards :cards="cards" :flipCard="flipCard" />
         <div class="mt-6" v-if="cards.length > 0">
             <h3 class="text-lg font-semibold mb-2">You times:</h3>
