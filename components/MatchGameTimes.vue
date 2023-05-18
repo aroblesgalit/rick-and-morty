ef7918274972afe33025e0fe5a898d6123b94a56
Alvin Galit <aroblesgalit@gmail.com>
2023-05-18 10:43:24 -0700

    Created component for match game times

---
+++ components/MatchGameTimes.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mt-6" v-if="cards.length > 0">
+        <h3 class="text-lg font-semibold mb-2">You times:</h3>
+        <div class="grid grid-cols-3 gap-3">
+            <div v-if="bestTimes.some(val => val.mode == 'easy')">
+                <h4>Easy</h4>
+                <div v-for="time in bestTimes" :key="time.id">
+                    <span v-if="time.mode == 'easy'">
+                        {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
+                    </span>
+                </div>
+            </div>
+            <div v-if="bestTimes.some(val => val.mode == 'medium')">
+                <h4>Medium</h4>
+                <div v-for="time in bestTimes" :key="time.id">
+                    <span v-if="time.mode == 'medium'">
+                        {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
+                    </span>
+                </div>
+            </div>
+            <div v-if="bestTimes.some(val => val.mode == 'hard')">
+                <h4>Hard</h4>
+                <div v-for="time in bestTimes" :key="time.id">
+                    <span v-if="time.mode == 'hard'">
+                        {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { cards, bestTimes } = defineProps(['cards', 'bestTimes'])
+</script>
+
+<style scoped>
+
+</style>
--- pages/games/matchgame.vue
+++ pages/games/matchgame.vue
@@ -2,35 +2,7 @@
     <div class="md:max-w-3xl mx-auto">
         <MatchGameHeader :timer="timer" :setCards="setCards" />
         <MatchGameCards :cards="cards" :flipCard="flipCard" />
-        <div class="mt-6" v-if="cards.length > 0">
-            <h3 class="text-lg font-semibold mb-2">You times:</h3>
-            <div class="grid grid-cols-3 gap-3">
-                <div v-if="bestTimes.some(val => val.mode == 'easy')">
-                    <h4>Easy</h4>
-                    <div v-for="time in bestTimes" :key="time.id">
-                        <span v-if="time.mode == 'easy'">
-                            {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
-                        </span>
-                    </div>
-                </div>
-                <div v-if="bestTimes.some(val => val.mode == 'medium')">
-                    <h4>Medium</h4>
-                    <div v-for="time in bestTimes" :key="time.id">
-                        <span v-if="time.mode == 'medium'">
-                            {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
-                        </span>
-                    </div>
-                </div>
-                <div v-if="bestTimes.some(val => val.mode == 'hard')">
-                    <h4>Hard</h4>
-                    <div v-for="time in bestTimes" :key="time.id">
-                        <span v-if="time.mode == 'hard'">
-                            {{ Math.floor(time.seconds / 60) }}m {{ (time.seconds % 60) < 10 ? '0' : '' }}{{ (time.seconds % 60) }}s
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <MatchGameTimes :cards="cards" :bestTimes="bestTimes" />
     </div>
     
 </template>
