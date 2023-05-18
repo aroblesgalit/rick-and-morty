c6ecfeecddaf4f5b6e8c988b933a5e601e264464
Alvin Galit <aroblesgalit@gmail.com>
2023-05-18 10:31:35 -0700

    Created component to hold the match game cards

---
+++ components/MatchGameCards.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="grid grid-cols-4 gap-3 md:gap-6">
+        <div 
+            v-for="card in cards" 
+            :id="card.id" 
+            :key="card.id" 
+            :class="[{flipped: card.flipped}]" 
+            class="card rounded-lg overflow-hidden shadow-md h-full relative bg-transparent cursor-pointer"
+            @click="flipCard(card)"
+        >
+            <MatchGameCard :card="card" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { cards, flipCard } = defineProps(['cards', 'flipCard'])
+</script>
+
+<style scoped>
+    .card {
+        perspective: 1000px;
+    }
+</style>
--- pages/games/matchgame.vue
+++ pages/games/matchgame.vue
@@ -8,26 +8,7 @@
             </div>
             <div>{{ Math.floor(timer / 60) }}m {{ (timer % 60) < 10 ? '0' : '' }}{{ (timer % 60) }}s</div>
         </div>
-        <div class="grid grid-cols-4 gap-3 md:gap-6">
-            <div 
-                v-for="card in cards" 
-                :id="card.id" 
-                :key="card.id" 
-                :class="[{flipped: card.flipped}]" 
-                class="card rounded-lg overflow-hidden shadow-md h-full relative bg-transparent cursor-pointer"
-                @click="flipCard(card)"
-            >
-                <MatchGameCard :card="card" />
-                <!-- <div class="card-inner relative w-full h-full text-center">
-                    <div class="card-front absolute w-full h-full">
-                        <img src="https://preview.redd.it/x498howiltl71.gif?format=png8&s=d1c048cf6e8e9863f60e390f628a75fffd592e08" alt="" class="w-full">
-                    </div>
-                    <div class="card-back">
-                        <img class="w-full" :src="card.image" alt="character card">
-                    </div>
-                </div> -->
-            </div>
-        </div>
+        <MatchGameCards :cards="cards" :flipCard="flipCard" />
         <div class="mt-6" v-if="cards.length > 0">
             <h3 class="text-lg font-semibold mb-2">You times:</h3>
             <div class="grid grid-cols-3 gap-3">
@@ -73,26 +54,5 @@ setCards('easy');
 </script>
 
 <style scoped>
-    .card {
-        perspective: 1000px;
-    }
-
-    /* .card-inner {
-        transition: transform 0.6s;
-        transform-style: preserve-3d;
-    }
-
-    .card.flipped .card-inner {
-        transform: rotateY(180deg);
-    }
-
-    .card-front,
-    .card-back {
-        -webkit-backface-visibility: hidden;
-        backface-visibility: hidden;
-    }
-
-    .card-back {
-        transform: rotateY(180deg);
-    } */
+  
 </style>
