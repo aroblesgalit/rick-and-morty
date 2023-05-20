54e3c4a312088fc3043a65d75f623e94db049b2c
Alvin Galit <aroblesgalit@gmail.com>
2023-05-19 22:42:00 -0700

    Created slot game page

---
+++ pages/games/slotgame.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="md:max-w-3xl mx-auto">
+        <div class="grid grid-cols-3 gap-6">
+            <div
+                v-for="token in tokens"
+                :id="token.id"
+                :key="token.id"
+                class="token rounded-lg overflow-hidden"
+            >
+                <img :src="token.image" alt="character card" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useSlotGameStore } from '~~/stores/slotGame';
+import { storeToRefs } from 'pinia';
+
+const slotGameStore = useSlotGameStore();
+const { setTokens } = slotGameStore;
+const { tokens } = storeToRefs(slotGameStore);
+
+setTokens();
+</script>
+
+<style scoped>
+
+</style>
