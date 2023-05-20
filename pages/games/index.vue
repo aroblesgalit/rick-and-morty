2facac77f6a4507367b41dfb82fe21e53b15c73d
Alvin Galit <aroblesgalit@gmail.com>
2023-05-19 22:42:43 -0700

    Added a tile for slot game

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
         <NuxtLink to="/games/matchgame"><GameTile title="Matching" color="#b2df28" /></NuxtLink>
+        <NuxtLink to="/games/slotgame"><GameTile title="Slot" color="#b2df28" /></NuxtLink>
     </div>
 </template>
 
